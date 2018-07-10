72784fd716370626989b9c8449f008b42261df24
Unknown <krzysztofwilczynski@mail.com>
2018-07-10 13:31:55 +0200

    Implemented modifyTask() definition, the API sketch is completed.

--- dbAPI.py
+++ dbAPI.py
@@ -18,14 +18,7 @@ _database = '/home/leon/Desktop/Cern_Project/SQL_DB/LHCb.db'
 # =====================================================================================================================
 
 def addTask(name, utgid, script, script_parameters = '', pcAdd_parameters = '', description = 'The description has not been specified.'):
-    path = _scripts + script
-    try:
-        conn = sqlite3.connect(_database)
-        c = conn.cursor()
-        c.execute("PRAGMA foreign_keys = ON")
-        print 'Connected to the database.' + os.linesep
-    except:
-        print 'ERROR: Couldn\'t connect to the database.' + os.linesep
+    connectDb()
 
 
     if(description == 'The description has not been specified.'):
@@ -43,39 +36,21 @@ def addTask(name, utgid, script, script_parameters = '', pcAdd_parameters = '', 
             pcAdd_parameters = raw_input('Please provide special parameters in the line below: \n')
             print 'Success.' + os.linesep
 
-    TASK = (name, utgid, path, script_parameters, pcAdd_parameters, description)
+    TASK = (name, utgid, script, script_parameters, pcAdd_parameters, description)
 
     try:
         c.execute('insert into Tasks values (?,?,?,?,?,?)', TASK)
         print 'The task has been added to the database.' + os.linesep
     except sqlite3.Error as e:
-        try:
-            print "Database Error [%d]: %s" % (e.args[0], e.args[1]) + os.linesep
-            return None
-        except IndexError:
-            print "Database Error: %s" % str(e) + os.linesep
-            return None
-    except TypeError, e:
-        print(e)
-        return None
-    except ValueError, e:
-        print(e)
-        return None
-    finally:
-        conn.commit()
-        conn.close()
+        handleDbError(e)
+    #finally:
+        #conn.commit()
+        #conn.close()
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 def deleteTask(name):
-
-    try:
-        conn = sqlite3.connect(_database)
-        c = conn.cursor()
-        c.execute("PRAGMA foreign_keys = ON")
-        print 'Connected to the database.' + os.linesep
-    except:
-        print 'ERROR: Couldn\'t connect to the database.'
+    c = connectDb()
 
     try:
         c.execute(''.join([
@@ -87,38 +62,142 @@ def deleteTask(name):
         ]))
         print 'The task has been deleted from the database.' + os.linesep
     except sqlite3.Error as e:
-        try:
-            print "Database Error [%d]: %s" % (e.args[0], e.args[1]) + os.linesep
-            return None
-        except IndexError:
-            print "Database Error: %s" % str(e) + os.linesep
-            return None
-    except TypeError, e:
-        print(e)
-        return None
-    except ValueError, e:
-        print(e)
-        return None
-
-
-    conn.commit()
-    conn.close()
+        handleDbError(e)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-def modifyTask():
-    print "Hello, not yet implemented."
+def modifyTask(name):
+    c = connectDb()
+
+    try:
+        c.execute(''.join([
+
+            'SELECT Tasks.task, Tasks.utgid, Tasks.command, Tasks.task_parameters, Tasks.command_parameters, Tasks.description ',
+            'FROM Tasks ',
+            'WHERE Tasks.task="', name, '\"',
+
+        ]))
+
+        i = 0
+        for row in c:
+            if i == 0:
+                print "The task you want to modify:" + os.linesep
+                FORMAT = '%-16s%-16s%-16s%-16s%-20s%-40s'
+                print FORMAT % ('task', 'utgid', 'command', 'parameters', 'pcAdd parameters', 'description')
+                print '-' * 100
+            print FORMAT % row
+            i += 1
+        print os.linesep
+
+    except sqlite3.Error as e:
+        handleDbError(e)
+
+    # Break if the specified task was not found in the database
+    if i == 0:
+        print 'The specified task was not found in the database, please check your spelling.' + os.linesep
+        return
+
+    try:
+        # See the properties of Tasks table, then iterate trough the column names and modify the records
+        c.execute(''.join([
+
+            'SELECT * ',
+            'FROM Tasks ',
+
+        ]))
+
+        # Dummy variable, needed for adressing the database row in SQL statements in the for loop
+        mod_task = ''
+        colnames = c.description
+        for row in colnames:
+            if row[0] == 'task':
+                if yes_or_no('Do you want to modify the name of the task?'):
+                    mod_task = raw_input("Please specify a new name (must be unique!): ")
+                    # First we need to modify the Tasks_to_Task_Sets table and drop the foreign key constraint between the tables
+                    c.execute("PRAGMA foreign_keys = OFF")
+                    c.execute(''.join([
+
+                        'UPDATE Tasks_to_Task_Sets ',
+                        'SET ',
+                        'task = "', mod_task, '\" ',
+                        'WHERE Tasks_to_Task_Sets.task = "', name, '\"',
+
+                    ]))
+                    # Now we can modify the Tasks table and bring back the constraint to prevent deleting tasks assigned to task sets
+                    c.execute(''.join([
+
+                        'UPDATE Tasks ',
+                        'SET ',
+                        'task = "', mod_task, '\" ',
+                        'WHERE Tasks.task = "', name, '\"',
+
+                    ]))
+                    c.execute("PRAGMA foreign_keys = ON")
+            elif row[0] == 'utgid':
+                if yes_or_no('Do you want to modify the utgid of the task?'):
+                    mod_utgid = raw_input("Please specify a new utgid: ")
+                    c.execute(''.join([
+
+                        'UPDATE Tasks ',
+                        'SET ',
+                        'utgid = "', mod_utgid, '\" ',
+                        'WHERE Tasks.task = "', name, '\"', 'OR Tasks.task = "', mod_task, '\"',
+
+                    ]))
+            elif row[0] == 'command':
+                if yes_or_no('Do you want to modify the script name of the task?'):
+                    mod_command = raw_input("Please specify a new script name: ")
+                    c.execute(''.join([
+
+                        'UPDATE Tasks ',
+                        'SET ',
+                        'command = "', mod_command, '\" ',
+                        'WHERE Tasks.task = "', name, '\"', 'OR Tasks.task = "', mod_task, '\"',
+
+                    ]))
+            elif row[0] == 'task_parameters':
+                if yes_or_no('Do you want to modify the parameters string of the task?'):
+                    mod_task_parameters = raw_input("Please specify a new parameters string: ")
+                    c.execute(''.join([
+
+                        'UPDATE Tasks ',
+                        'SET ',
+                        'task_parameters = "', mod_task_parameters, '\" ',
+                        'WHERE Tasks.task = "', name, '\"', 'OR Tasks.task = "', mod_task, '\"',
+
+                    ]))
+            elif row[0] == 'command_parameters':
+                if yes_or_no('Do you want to modify the additional pcAdd parameters associated with the task?'):
+                    mod_command_parameters = raw_input("Please specify a new pcAdd parameters string: ")
+                    c.execute(''.join([
+
+                        'UPDATE Tasks ',
+                        'SET ',
+                        'command_parameters = "', mod_command_parameters, '\" ',
+                        'WHERE Tasks.task = "', name, '\"', 'OR Tasks.task = "', mod_task, '\"',
+
+                    ]))
+            elif row[0] == 'description':
+                if yes_or_no('Do you want to modify the description of the task?'):
+                    mod_description = raw_input("Please specify a new description: ")
+                    c.execute(''.join([
+
+                        'UPDATE Tasks ',
+                        'SET ',
+                        'description = "', mod_description, '\" ',
+                        'WHERE Tasks.task = "', name, '\"', 'OR Tasks.task = "', mod_task, '\"',
+
+                    ]))
+
+        print os.linesep + 'The task has modified.' + os.linesep
+
+    except sqlite3.Error as e:
+        handleDbError(e)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 def selectTask(name):
-    try:
-        conn = sqlite3.connect(_database)
-        c = conn.cursor()
-        c.execute("PRAGMA foreign_keys = ON")
-        print 'Connected to the database.' + os.linesep
-    except:
-        print 'ERROR: Couldn\'t connect to the database.'
+    c = connectDb()
 
     try:
         c.execute(''.join([
@@ -140,21 +219,36 @@ def selectTask(name):
         print os.linesep
 
     except sqlite3.Error as e:
-        try:
-            print "Database Error [%d]: %s" % (e.args[0], e.args[1]) + os.linesep
-            return None
-        except IndexError:
-            print "Database Error: %s" % str(e) + os.linesep
-            return None
-        except TypeError, e:
-            print(e)
-            return None
-        except ValueError, e:
-            print(e)
-            return None
+        handleDbError(e)
 
-    conn.commit()
-    conn.close()
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def connectDb():
+    try:
+        conn = sqlite3.connect(_database)
+        c = conn.cursor()
+        c.execute("PRAGMA foreign_keys = ON")
+        print 'Connected to the database.' + os.linesep
+        return c
+    except:
+        print 'ERROR: Couldn\'t connect to the database.'
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def handleDbError(e):
+    try:
+        print "Database Error [%d]: %s" % (e.args[0], e.args[1]) + os.linesep
+        return None
+    except IndexError:
+        print "Database Error: %s" % str(e) + os.linesep
+        return None
+    except TypeError, e:
+        print(e)
+        return None
+    except ValueError, e:
+        print(e)
+        return None
 
 # ---------------------------------------------------------------------------------------------------------------------
 
