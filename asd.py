0b2d0710d1f3e77541fc31536ab00917dabf48ba
Unknown <krzysztofwilczynski@mail.com>
2018-07-11 11:00:52 +0200

    Verified the functionality of addTaskSet(), began implementing further API functionalities.

--- asd.py
+++ asd.py
@@ -17,22 +17,29 @@ _database = '/home/leon/Desktop/Cern_Project/SQL_DB/LHCb.db'
 #                              TASKS
 # =====================================================================================================================
 
-def addTask(name, utgid, script, script_parameters = '', pcAdd_parameters = '', description = 'The description has not been specified.'):
-    connectDb()
+def addTask(name, utgid = '', script = '', script_parameters = '', pcAdd_parameters = '', description = 'The description has not been specified.'):
+    c = connectDb()
 
-
+    if (utgid == ''):
+        if (yes_or_no('The required parameter - utgid of the task has not been specified, do you want to do this now?', 'yes')):
+            utgid = raw_input('Please provide the utgid for your task: \n')
+            print 'Success.' + os.linesep
+    if (script == ''):
+        if (yes_or_no('The required parameter - script name of the task has not been specified, do you want to do this now?', 'yes')):
+            script = raw_input('Please provide the script name for your task: \n')
+            print 'Success.' + os.linesep
     if(description == 'The description has not been specified.'):
-        if(yes_or_no('The description of the task has not been specified, do you want to do this now?')):
+        if(yes_or_no('The description of the task has not been specified, do you want to do this now?', 'no')):
             description = raw_input('Please provide the description for your task: \n')
             print 'Success.' + os.linesep
 
     if (script_parameters == ''):
-        if (yes_or_no('The parameters string for the task has not been specified, do you want to do this now?')):
+        if (yes_or_no('The parameters string for the task has not been specified, do you want to do this now?', 'no')):
             script_parameters = raw_input('Please provide the parameters for your script in the line below: \n')
             print 'Success.' + os.linesep
 
     if (pcAdd_parameters == ''):
-        if (yes_or_no('No special parameters for the pcAdd command has been specified, do you want to do this now?')):
+        if (yes_or_no('No special parameters for the pcAdd command has been specified, do you want to do this now?', 'no')):
             pcAdd_parameters = raw_input('Please provide special parameters in the line below: \n')
             print 'Success.' + os.linesep
 
@@ -111,7 +118,7 @@ def modifyTask(name):
         colnames = c.description
         for row in colnames:
             if row[0] == 'task':
-                if yes_or_no('Do you want to modify the name of the task?'):
+                if yes_or_no('Do you want to modify the name of the task?', 'no'):
                     mod_task = raw_input("Please specify a new name (must be unique!): ")
                     # First we need to modify the Tasks_to_Task_Sets table and drop the foreign key constraint between the tables
                     c.execute("PRAGMA foreign_keys = OFF")
@@ -134,7 +141,7 @@ def modifyTask(name):
                     ]))
                     c.execute("PRAGMA foreign_keys = ON")
             elif row[0] == 'utgid':
-                if yes_or_no('Do you want to modify the utgid of the task?'):
+                if yes_or_no('Do you want to modify the utgid of the task?', 'no'):
                     mod_utgid = raw_input("Please specify a new utgid: ")
                     c.execute(''.join([
 
@@ -145,7 +152,7 @@ def modifyTask(name):
 
                     ]))
             elif row[0] == 'command':
-                if yes_or_no('Do you want to modify the script name of the task?'):
+                if yes_or_no('Do you want to modify the script name of the task?', 'no'):
                     mod_command = raw_input("Please specify a new script name: ")
                     c.execute(''.join([
 
@@ -156,7 +163,7 @@ def modifyTask(name):
 
                     ]))
             elif row[0] == 'task_parameters':
-                if yes_or_no('Do you want to modify the parameters string of the task?'):
+                if yes_or_no('Do you want to modify the parameters string of the task?', 'no'):
                     mod_task_parameters = raw_input("Please specify a new parameters string: ")
                     c.execute(''.join([
 
@@ -167,7 +174,7 @@ def modifyTask(name):
 
                     ]))
             elif row[0] == 'command_parameters':
-                if yes_or_no('Do you want to modify the additional pcAdd parameters associated with the task?'):
+                if yes_or_no('Do you want to modify the additional pcAdd parameters associated with the task?', 'no'):
                     mod_command_parameters = raw_input("Please specify a new pcAdd parameters string: ")
                     c.execute(''.join([
 
@@ -178,7 +185,7 @@ def modifyTask(name):
 
                     ]))
             elif row[0] == 'description':
-                if yes_or_no('Do you want to modify the description of the task?'):
+                if yes_or_no('Do you want to modify the description of the task?', 'no'):
                     mod_description = raw_input("Please specify a new description: ")
                     c.execute(''.join([
 
@@ -221,6 +228,122 @@ def selectTask(name):
     except sqlite3.Error as e:
         handleDbError(e)
 
+# =====================================================================================================================
+#                             TASK SETS
+# =====================================================================================================================
+
+def addTaskSet(task_set, description = 'The description has not been specified.'):
+    c = connectDb()
+
+    try:
+        nexttask = 1
+        if yes_or_no('Do you want to add tasks to this new task set?', 'yes'):
+            while nexttask == 1:
+                addtask = raw_input('Specify the name of the task: ')
+                # Check if the task exists in the Tasks table, if it does, add it
+                c.execute('SELECT Tasks.task FROM Tasks WHERE task="' + addtask + '\"')
+                data = c.fetchall()
+                if len(data) == 0:
+                    if yes_or_no("The specified task doesn't exist in the database, do you want to define it now?", 'yes'):
+                        print 'Use addtask(task) command to add a new task.'
+                    else:
+                        print "The non existant task is not will not be added to the set." + os.linesep
+                        if yes_or_no('Do you want to add another task?', 'no'):
+                            nexttask = 1
+                        else:
+                            nexttask = 0
+                # The task exists in the Tasks table, so it can be added to a set safely
+                else:
+                    c.execute("PRAGMA foreign_keys = OFF")
+                    c.execute('insert into Tasks_to_Task_Sets values (?,?)', (addtask, task_set))
+                    c.execute("PRAGMA foreign_keys = ON")
+                    if yes_or_no('Do you want to add another task?', 'no'):
+                        nexttask = 1
+                    else:
+                        nexttask = 0
+        else:
+            # No task exists and no task has been added - only then the nexttask variable remains set to 1
+            if nexttask == 1:
+                print "Can't create an empty task set, breaking." + os.linesep
+                return
+
+        if description == 'The description has not been specified.':
+            if yes_or_no('The description of the task has not been specified, do you want to do this now?', 'no'):
+                description = raw_input('Please provide the description for your task: \n')
+                print 'Success.' + os.linesep
+
+        SET = (task_set, description)
+        c.execute("PRAGMA foreign_keys = OFF")
+        c.execute('insert into Task_Sets values (?,?)', SET)
+        c.execute("PRAGMA foreign_keys = ON")
+        print 'The task set has been added to the database.' + os.linesep
+
+    except sqlite3.Error as e:
+        handleDbError(e)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def deleteTaskSet(task_set):
+    c = connectDb()
+
+    # TODO: check if the task set is assigned to a node class, prompt the user if he is sure, if yes, delete the binding
+
+    try:
+        c.execute("PRAGMA foreign_keys = OFF")
+        c.execute(''.join([
+
+            'DELETE * ',
+            'FROM Task_Sets ',
+            'WHERE Task_Sets.task_set="', task_set, '\"',
+
+        ]))
+        print 'The task_set has been deleted from the database.' + os.linesep
+        c.execute("PRAGMA foreign_keys = ON")
+
+    except sqlite3.Error as e:
+        handleDbError(e)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def modifyTaskSet():
+    return
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def selectTaskSet():
+    return
+
+# =====================================================================================================================
+#                           NODE CLASSES
+# =====================================================================================================================
+
+def addClass():
+    return
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def deleteClass():
+    return
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def modifyClass():
+    return
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def selectClass():
+    return
+
+# =====================================================================================================================
+#                              NODES
+# =====================================================================================================================
+
+def addNodes():
+    return
+# =====================================================================================================================
+#                              OTHERS
+# =====================================================================================================================
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -253,15 +376,9 @@ def handleDbError(e):
 # ---------------------------------------------------------------------------------------------------------------------
 
 def yes_or_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
 
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
+    #Ask a question specified, default answer is executed if the user hits enter, returns true or false
 
-    The "answer" return value is True for "yes" or False for "no".
-    """
     valid = {"yes": True, "y": True, "ye": True,
              "no": False, "n": False}
     if default is None:
@@ -287,6 +404,9 @@ def yes_or_no(question, default="yes"):
 # ---------------------------------------------------------------------------------------------------------------------
 
 def LHCb():
+
+    # Just a nice easter egg :)
+
     from pyfiglet import Figlet
     #f = Figlet(font='speed')
     f = Figlet(font='slant')
