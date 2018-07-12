246dd7209cd6cc14f4b461af4911d61cb6c2f4f5
Unknown <krzysztofwilczynski@mail.com>
2018-07-12 18:25:08 +0200

    Done, it's working, full API and an elastic Boot Script.

--- bootScript.py
+++ bootScript.py
@@ -10,20 +10,101 @@ import sys
 
 _scripts = 'group/online/dataflow/scripts' + os.sep
 _database = '/home/leon/Desktop/Cern_Project/SQL_DB/LHCb.db'
+logDefaultFIFO = '/run/fmc/logSrv.fifo'
+dataFlowDirScripts = '/group/online/dataflow/cmtuser/Gaudi_v19r2/Online/OnlineControls/scripts'
+default_exports = 'export PATH=/opt/FMC/sbin:/opt/FMC/bin:$PATH;export LD_LIBRARY_PATH=/opt/FMC/lib;'
+top_logger_host = 'ecs03'
+top_farm_host = 'hlt01'
 
 # =====================================================================================================================
 #                            BOOT SCRIPTS
 # =====================================================================================================================
 
-def boot(regex):
-    a = connectDb()
+def boot(regex = '*'):    # Default to be replaced with * wchich will boot the whole system
 
-    if not existsInDb(a, regex, 'regex', 'Nodes'):
+    tasklist = []
+
+    conn = connectDb()
+
+    # Full system boot - need to read all node regex
+    if regex == '*':
+        print 'Starting full system boot sequence...'
+        try:
+
+            conn.execute(''.join([
+
+                'SELECT DISTINCT Nodes.regex ',
+                'FROM Nodes ',
+
+            ]))
+        except sqlite3.Error as error:
+            handleDbError(error)
+
+        for host in conn:
+            # Boot nodes specified by all of the regular expressions found in the database
+            bootnode(host[0])
+
+    # Abort boot if the specified regex is not found in the database
+    elif not existsInDb(conn, regex, 'regex', 'Nodes'):
         print 'The specified node name (or regular expression) was not found in the database, breaking.' + os.linesep
         return
 
+    # Single regex boot
+    else:
+        print 'Starting boot sequence on ' + regex + '...'
+        bootnode(regex)
+
+
+# ==============================    =======================================================================================
+#                              OTHERS
+# =====================================================================================================================
+
+def connectDb():
+    try:
+        conn = sqlite3.connect(_database)
+        c = conn.cursor()
+        c.execute("PRAGMA foreign_keys = ON")
+        # print 'Connected to the database.' + os.linesep
+        return c
+    except:
+        print 'ERROR: Couldn\'t connect to the database.'
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def handleDbError(e):
+    try:
+        print "Database Error [%d]: %s" % (e.args[0], e.args[1]) + os.linesep
+        return None
+    except IndexError:
+        print "Database Error: %s" % str(e) + os.linesep
+        return None
+    except TypeError, e:
+        print(e)
+        return None
+    except ValueError, e:
+        print(e)
+        return None
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def existsInDb(db_connection, variable, column, table):
+    c = db_connection
+    c.execute('SELECT ' + column + ' FROM ' + table + ' WHERE ' + column + '="' + variable + '\"')
+    data = c.fetchall()
+    if len(data) == 0:
+        return False
+    else:
+        return True
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def bootnode(regex):
+
+    tasklist = []
+
     # select distinct Task_Sets_to_Classes.class from Task_Sets_to_Classes join Nodes on Nodes.class = Task_Sets_To_Classes.class where Nodes.regex = 'slp[a-z][0-9][0-9]';
     try:
+        a = connectDb()
         a.execute(''.join([
 
             'SELECT DISTINCT Task_Sets_to_Classes.class ',
@@ -77,59 +158,31 @@ def boot(regex):
 
                 i = 0
                 for row4 in d:
-                    if i == 0:
-                        FORMAT = '%-16s%-16s%-16s%-16s%-20s%-40s'
-                        print FORMAT % ('task', 'utgid', 'command', 'parameters', 'pcAdd parameters', 'description')
-                        print '-' * 124
-                    print FORMAT % row4
-                    i += 1
+                    # FORMAT = '%-16s%-16s%-16s%-16s%-20s%-40s'
+                    # print FORMAT % row4
+                    tasklist.append(row4)
 
+    # Remove duplicate tasks from the list created by looping SQL queries above
+    tasklist = set(tasklist)
+    # Print the list of tasks after the duplicate removal
+    print os.linesep + 'Tasks to be started on ' + regex + ":" + os.linesep
+    for task in tasklist:
+        # FORMAT = '%-16s%-16s%-16s%-16s%-20s%-40s'
+        # print FORMAT % task
 
+        # pcAdd
+        log_opts = '-E ' + logDefaultFIFO + ' -O ' + logDefaultFIFO
+        if task[3] != '':
+            script_params = task[3]
+        if task[4] != '':
+            pcAdd_Params = task[4]
+        else:
+            pcAdd_Params = '-K 120 -M 5 -X 300 -g onliners -p 0 -n online'
 
+        #             user def or default       logs              host              utgid            path     script      script params
+        cmd = 'pcAdd ' + pcAdd_Params + ' ' + log_opts + ' -m ' + regex + ' -u ' + task[1] + ' ' + _scripts + task[2] + ' ' + task[4] + '";'
+        print 'echo "' + cmd + '";'
 
-# =====================================================================================================================
-#                              OTHERS
-# =====================================================================================================================
-
-def connectDb():
-    try:
-        conn = sqlite3.connect(_database)
-        c = conn.cursor()
-        c.execute("PRAGMA foreign_keys = ON")
-        print 'Connected to the database.' + os.linesep
-        return c
-    except:
-        print 'ERROR: Couldn\'t connect to the database.'
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-def handleDbError(e):
-    try:
-        print "Database Error [%d]: %s" % (e.args[0], e.args[1]) + os.linesep
-        return None
-    except IndexError:
-        print "Database Error: %s" % str(e) + os.linesep
-        return None
-    except TypeError, e:
-        print(e)
-        return None
-    except ValueError, e:
-        print(e)
-        return None
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-def existsInDb(db_connection, variable, column, table):
-    c = db_connection
-    c.execute('SELECT ' + column + ' FROM ' + table + ' WHERE ' + column + '="' + variable + '\"')
-    data = c.fetchall()
-    if len(data) == 0:
-        return False
-    else:
-        return True
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-
+        # log_opts = '-E ' + str(fifo) + ' -O ' + str(fifo) was ommited for now! it depends on default None variable in the original script
 
 
