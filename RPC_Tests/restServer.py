4f3b333fd1e82871875de244a4d03a5c970ed85e
Unknown <krzysztofwilczynski@mail.com>
2018-07-26 13:45:46 +0200

    Added REST API test script for the database...

---
+++ RPC_Tests/restServer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask_jsonpify import jsonify
+
+db_connect = create_engine('sqlite:///../LHCb.db')
+app = Flask(__name__)
+api = Api(app)
+
+
+class Tasks(Resource):
+    def get(self):
+        conn = db_connect.connect()  # connect to database
+        query = conn.execute("select * from Tasks")  # This line performs query and returns json result
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        #return {'Tasks': [i[0] for i in query.cursor.fetchall()]}  # Fetches first column that is Employee ID
+        return jsonify(result)
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return jsonify(result)
+
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from employees where EmployeeId =%d " % int(employee_id))
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return jsonify(result)
+
+
+api.add_resource(Tasks, '/tasks')  # Route_1
+api.add_resource(Tracks, '/tracks')  # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>')  # Route_3
+
+if __name__ == '__main__':
+    app.run(port='5002')
