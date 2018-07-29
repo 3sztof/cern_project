fc910a8e7622a46f39693a176013a85cbb5c8500
Unknown <krzysztofwilczynski@mail.com>
2018-07-29 15:43:37 +0200

    Nothing special...

--- Sencha_App/classic/src/view/main/Main.js
+++ Sencha_App/classic/src/view/main/Main.js
@@ -6,99 +6,51 @@
  * TODO - Replace this content of this view to suite the needs of your application.
  */
 Ext.define('LHCb.view.main.Main', {
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.Container',
     xtype: 'app-main',
 
-    requires: [
-        'Ext.plugin.Viewport',
-        'Ext.window.MessageBox',
+    
 
-        'LHCb.view.main.MainController',
-        'LHCb.view.main.MainModel',
-        'LHCb.view.main.List'
-    ],
-
-    controller: 'main',
-    viewModel: 'main',
-
-    ui: 'navigation',
-
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
-    tabRotation: 0,
-
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
+    layout: {
+        type: 'vbox',
+        pack: 'center',
+        align: 'stretch'
     },
-
-    tabBar: {
-        flex: 1,
-        layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
-    },
-
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
-        wide: {
-            headerPosition: 'left'
-        }
-    },
-
+    margin: '0 10',
     defaults: {
-        bodyPadding: 20,
-        tabConfig: {
-            plugins: 'responsive',
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
-        }
+        margin: '0 0 10 0',
+        bodyPadding: 10
     },
-
-    items: [{
-        title: 'Home',
-        iconCls: 'fa-home',
-        // The following grid shares a store with the classic version's grid as well!
-        items: [{
-            xtype: 'mainlist'
+    items: [
+        {
+            layout: {
+                type: 'hbox',
+                pack: 'center',
+                align: 'stretch'
+            },
+            defaults: {
+                flex: 1,
+                bodyPadding: 10,
+                html: 'test'
+            },
+            items: [
+                {
+                    xtype: 'panel',
+                    margin: '0 5 0 0'
+                },
+                {
+                    xtype: 'panel',
+                    title: 'Title',
+                    margin: '0 0 0 5'
+                }
+            ]
+        },
+        {
+            xtype: 'panel',
+            title: 'Built in Tools',
+            html: 'Lorem ipsum',
+            tools: [
+                {type: 'minimize'},
+            ]
         }]
-    }, {
-        title: 'Users',
-        iconCls: 'fa-user',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Groups',
-        iconCls: 'fa-users',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Settings',
-        iconCls: 'fa-cog',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }]
-});
+    });
